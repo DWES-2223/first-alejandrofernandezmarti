1e3bf62b82105a0b9e791f2a0dabe81659456e85
alejandrofernandezmarti <alefermar16@alu.edu.gva.es>
2023-09-25 20:07:46 +0200

    sr

--- php/src/202.php
+++ php/src/202.php
@@ -1,1 +1,42 @@
 <?php
+$nom  = "Ale";
+$cognoms = "Fe";
+$email = "alejand";
+$natalici = "200";
+$telefono = "6998";
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabla HTML</title>
+</head>
+<body>
+    <table border="1">
+            <tr>
+                <td>Nom</td>
+                <td>Ignasi</td>
+            </tr>
+
+            <tr>
+                <td>Cognoms</td>
+                <td><?=$cognoms?></td>
+            </tr>
+        <tr>
+            <td>Email</td>
+            <td><?=$email?></td>
+        </tr>
+        <tr>
+            <td>Natalici</td>
+            <td><?=$natalici?></td>
+        </tr>
+            <tr>
+                <td>Telèfon</td>
+                <td><?=$telefono?></td>
+            </tr>
+    </table>
+</body>
+</html>
